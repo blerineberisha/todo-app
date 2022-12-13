a48c6558e7df265d4d581bb2f8433fc6573f98ee
alishakhalid <aalishakhalid@gmail.com>
2022-12-13 10:46:48 +0100

    added data for database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,15 +9,29 @@ VALUE
     (2, 'SEE_OWN_TASKS'),
     (3, 'SEE_ALL_TASKS'),
     (4, 'SEE_ALL_TODOLIST'),
-    (5, 'SEE_OWN_TODOLIST');
+    (5, 'SEE_OWN_TODOLIST'),
+    (6, 'CREATE_TODOLIST'),
+    (7, 'CREATE_TASKS'),
+    (8, 'DELETE_TODOLIST'),
+    (9, 'DELETE_TASK'),
+    (10, 'EDIT_TASK'),
+    (11, 'EDIT_TODOLIST'),
+    (12, 'EDIT_USER');
 
-insert into vinosfue_todoapp.role_authority(id_role, id_authority)
+insert into vinosfue_todoapp.role_authority(role_id, authority_id)
 VALUE
     (1, 1),
     (1, 3),
     (1, 4),
+    (1, 12),
     (2, 2),
-    (2, 5);
+    (2, 5),
+    (2, 6),
+    (2, 7),
+    (2, 8),
+    (2, 9),
+    (2, 10),
+    (2, 11);
 
 insert into users(users_id, email, first_name, last_name, password, username, id_role)
 VALUE
@@ -26,10 +40,12 @@ VALUE
 
 
 insert into todo_list(todo_list_id, name, id_users)
-           VALUE
-           (1, 'School', 2);
+VALUE
+    (1, 'School', 2),
+    (2, 'Work', 2);
 
 insert into task(task_id, description, name, state, id_todo_list)
     VALUE
-    (1, 'Write book summary', 'English', 'OPEN', 1)
-    (2, 'Create base structure of webshop', 'IT', 'OPEN', 1);
+    (1, 'Write book summary', 'English', 'OPEN', 1),
+    (2, 'Create base structure of webshop', 'IT', 'OPEN', 1),
+    (3, 'Create get and put endpoint for user', 'User Endpoints', 'IN_PROGRESS', 2);
