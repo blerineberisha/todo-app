a78ba2f6f57243ce4237a4aba7e4a406b9ccef08
alishakhalid <aalishakhalid@gmail.com>
2022-12-13 09:33:03 +0100

    referenced foreign keys and changed attribute name

--- src/main/java/m426/todoapp/todoappbe/todo/TodoList.java
+++ src/main/java/m426/todoapp/todoappbe/todo/TodoList.java
@@ -9,6 +9,7 @@ import m426.todoapp.todoappbe.task.Task;
 
 import java.util.HashSet;
 import java.util.Set;
+import m426.todoapp.todoappbe.users.Users;
 
 @Entity
 @Getter
@@ -19,11 +20,14 @@ import java.util.Set;
 public class TodoList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "todoList_id", nullable = false)
+    @Column(name = "todo_list_id", nullable = false)
     int todoListId;
     @Column(name = "name", length = 20)
     String name;
     @OneToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, mappedBy = "todoList")
     @JsonBackReference
     Set<Task> tasks = new HashSet<>();
+    @ManyToOne
+    @JoinColumn(name = "id_users", referencedColumnName = "users_id", nullable = false)
+    Users user;
 }
