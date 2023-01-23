2e7f540679a0779ecce3503bf6aec9e48aecda31
Blerinë <blerineberisha@gmail.com>
2023-01-23 19:45:16 +0100

    fixed gitignore, removed preAuthorize from controller, uncommented security dependencies in build.grade

--- src/main/java/m426/todoapp/todoappbe/task/TaskController.java
+++ src/main/java/m426/todoapp/todoappbe/task/TaskController.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,7 +15,6 @@ public class TaskController {
     private final TaskService taskService;
 
     @PutMapping("/{id}")
-    //@PreAuthorize("permitAll()")
     public ResponseEntity<Task> updateTask(@PathVariable("id") int id, @RequestBody Task task){
         return new ResponseEntity<>(taskService.updateTask(task, id), HttpStatus.OK);
     }
