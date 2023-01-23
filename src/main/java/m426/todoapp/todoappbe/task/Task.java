23de623623fd9b5617100b272217d7b2a0c433e7
Blerinë <blerineberisha@gmail.com>
2023-01-23 19:26:30 +0100

    merged develop

--- src/main/java/m426/todoapp/todoappbe/task/Task.java
+++ src/main/java/m426/todoapp/todoappbe/task/Task.java
@@ -2,7 +2,7 @@ package m426.todoapp.todoappbe.task;
 
 import javax.persistence.*;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import m426.todoapp.todoappbe.state.StateType;
@@ -26,7 +26,7 @@ public class Task {
     @Enumerated(EnumType.STRING)
     StateType state;
     @ManyToOne(optional = false, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
-    @JsonIgnore
+    @JsonBackReference
     @JoinColumn(name = "id_todo_list", referencedColumnName = "todo_list_id", nullable = false)
     TodoList todoList;
 }
